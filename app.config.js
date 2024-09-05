f962db6cdfefc75bd1593a70428534fa681f48ff
salman7355 <salmanmohamed837@gmail.com>
2024-09-05 22:25:22 +0300

    stories

---
+++ app.config.js
@@ -0,0 +1,55 @@
+export default {
+  expo: {
+    name: "test",
+    slug: "instalink",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/InstaLink.jpg",
+      resizeMode: "contain",
+      backgroundColor: "#191a1c",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          microphonePermission:
+            "Allow $(PRODUCT_NAME) to access your microphone",
+          recordAudioAndroid: true,
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+
+      eas: {
+        projectId: process.env.EXPO_PUBLIC_PROJECTID,
+      },
+    },
+    owner: "salman7355",
+  },
+};
